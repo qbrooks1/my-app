6fc19fd068ab32ec655919368175995a128fe8e2
Quinton <qbrooksii@gmail.com>
2023-02-04 19:13:44 -0600

    [Commit 1]: Initial Form Page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
 function App() {
+  // function get() {
+  //   axios.get("https://frontend-take-home.fetchrewards.com/form").then(({data}) => {
+  //     setText(JSON.stringify(data));
+  //   });
+  // }
+
+  // function post() {
+  //   axios.post("https://frontend-take-home.fetchrewards.com/form").then(({}) => {
+  //     setText(JSON.stringify(data));
+  //   })
+  // }
+
+  const [values, setValues] = React.useState({
+    password: "",
+    showPassword: false,
+  });
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form className="form">
+          <label className="label"> Full Name
+            <input type="text" name="name" className="input"/>
+          </label>
+          <label className="label"> Email
+            <input type="text" className="input"/>
+          </label>
+          <label className="label"> Password
+            <input type={values.showPassword ? "text" : "password"} className="input"/>
+          </label>
+          <label className="label"> Occupation
+            <input type="text" className="input"/>
+          </label>
+          <label className="label"> State
+            <input type="text" className="input"/>
+          </label>
+          <button type="submit" className="submitButton">Submit</button>
+        </form>
       </header>
     </div>
   );
